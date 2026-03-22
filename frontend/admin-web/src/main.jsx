2fe06a1396cffa6b6d38b96b7c8aadd0bc02ac33
Maverick Barrientos <maverickjadebarrientos@gmail.com>
2026-03-23 00:55:01 +0800

    fixed path error

--- frontend/admin-web/src/main.jsx
+++ frontend/admin-web/src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { QRProvider } from './contexts/qrCodeContext.jsx'
 import { AuthProvider } from './contexts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QRProvider>
-      <AuthProvider>
-        <App/>
-      </AuthProvider>
-    </QRProvider>
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
   </StrictMode>,
 )
